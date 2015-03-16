54c93d6e064cd34a4bfc2307f5e2dcee5ff06168
Thomas McNiven <hello@vevix.net>
2015-03-16 11:52:04 +1000

    initial commit

---
+++ link.rb
@@ -0,0 +1,26 @@
+require "fileutils"
+
+TABLE = {
+  "~/.gemrc" => "linked/.gemrc",
+  "~/.gitconfig" => "linked/.gitconfig",
+  "~/.gitexcludes" => "linked/.gitexcludes",
+  "~/.gitignore" => "linked/.gitignore",
+  "~/.inputrc" => "linked/.inputrc",
+  "~/.pryrc" => "linked/.pryrc",
+  "~/.rbenv/default-gems" => "linked/.rbenv/default-gems",
+  "~/.rspec" => "linked/.rspec",
+  "~/.zshrc" => "linked/.zshrc"
+}.inject({}) do |result, (key, value)|
+  result.merge(File.expand_path(key) => File.expand_path(value))
+end
+
+def install
+  system("git pull")
+  TABLE.each do |dest, source|
+    File.symlink(source, dest) rescue nil
+  end
+end
+
+def uninstall
+  FileUtils.rm(TABLE.keys)
+end
