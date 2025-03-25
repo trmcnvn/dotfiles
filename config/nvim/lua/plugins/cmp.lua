0d3c400a8bececd13e9479b699f9a1b1b7d75bca
trmcnvn <hello@trmcnvn.dev>
2025-03-26 06:58:02 +1100

    Updates

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -91,22 +91,6 @@ return {
 					},
 				},
 			})
-
-			-- Copilot, hide suggestions when BlinkCmp menu is open
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "BlinkCmpMenuOpen",
-				callback = function()
-					require("copilot.suggestion").dismiss()
-					vim.b.copilot_suggestion_hidden = true
-				end,
-			})
-
-			vim.api.nvim_create_autocmd("User", {
-				pattern = "BlinkCmpMenuClose",
-				callback = function()
-					vim.b.copilot_suggestion_hidden = false
-				end,
-			})
 		end,
 	},
 }
--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -2,7 +2,6 @@ return {
 	{
 		"zbirenbaum/copilot.lua",
 		event = { "BufNewFile" },
-		commit = "825b6a9574584c5f90a4abcdf04ebb98f2c1260a",
 		config = function()
 			require("copilot").setup({
 				panel = { enabled = false },
@@ -10,48 +9,5 @@ return {
 				copilot_model = "gpt-4o-copilot",
 			})
 		end,
-	},
-	{
-		"yetone/avante.nvim",
-		lazy = true,
-		version = false,
-		build = "make",
-		keys = {
-			{ "<leader>aa", "<cmd>AvanteToggle<CR>" },
-		},
-		opts = {
-			provider = "copilot",
-			auto_suggestions_provider = "copilot",
-			copilot = {
-				model = "claude-3.7-sonnet",
-			},
-			behaviour = {
-				auto_suggestions = false,
-			},
-			hints = { enabled = false },
-			windows = {
-				width = 40,
-				wrap = true,
-			},
-			file_selector = {
-				provider = "snacks",
-				provider_opts = {},
-			},
-		},
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"stevearc/dressing.nvim",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			"echasnovski/mini.icons",
-			{
-				"MeanderingProgrammer/render-markdown.nvim",
-				opts = {
-					file_types = { "markdown", "Avante" },
-				},
-				ft = { "markdown", "Avante" },
-				latex = { enabled = false },
-			},
-		},
 	},
 }
