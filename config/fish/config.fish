cc4568d9795d0262087890ff56f48d9f9513eecf
trmcnvn <hello@trmcnvn.dev>
2025-03-27 06:42:16 +1100

    Update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,76 +1,84 @@
-fish_add_path /opt/homebrew/bin
-fish_add_path /home/linuxbrew/.linuxbrew/bin
-
-set fish_greeting
+# Path Configuration
 set -l machine_name (uname)
+fish_add_path -g \
+    /opt/homebrew/bin \
+    /home/linuxbrew/.linuxbrew/bin \
+    /usr/local/sbin \
+    $HOME/.local/bin \
+    $HOME/.cargo/bin \
+    $HOME/.npm-global/bin \
+    $HOME/.nimble/bin \
+    $HOME/.dotnet/bin \
+    $(go env GOPATH)/bin
 
-zoxide init fish | source
+# OS-specific paths
+if test $machine_name = Darwin
+    fish_add_path -g \
+        "$HOME/Library/Application Support/JetBrains/Toolbox/scripts" \
+        /Applications/Ghostty.app/Contents/MacOS
+end
 
-# Aliases
-alias cl=clear
-alias ..="cd .."
-alias gbra="git branch"
-alias gdif="git diff --no-prefix"
-alias glog="git log --color --oneline"
-alias gst="git status"
-alias gwl="git worktree list"
-alias gwa="git worktree add"
-alias gwr="git worktree remove"
-alias gwp="git worktree prune"
-alias ls="lsd"
-alias cat="bat"
-alias lg="lazygit"
-alias cd="z"
-alias vim="nvim"
-
-# ENV
+# Environment Variables
 set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_STATE_HOME $HOME/.local/state
-set -gx EDITOR "nvim"
-set -gx PNPM_HOME $HOME/Library/pnpm
-set -gx DOTNET_ROOT "/opt/homebrew/opt/dotnet/libexec"
-set -gx GPG_TTY $(tty)
+set -gx EDITOR nvim
 set -gx CARGO_HOME $XDG_DATA_HOME/cargo
+set -gx PNPM_HOME $HOME/Library/pnpm
+set -gx DOTNET_ROOT /opt/homebrew/opt/dotnet/libexec
+set -gx GPG_TTY (tty)
 set -gx RUST_WITHOUT rust-docs
 set -gx JJ_CONFIG $XDG_CONFIG_HOME/jj/config.toml
-test machine_name = Darwin; and set -gx TERM xterm-256color
+set -gx BUN_INSTALL $HOME/.bun
 
-fish_add_path $PNPM_HOME
-fish_add_path /usr/local/sbin
-fish_add_path $HOME/.cargo/bin
-test machine_name = Darwin; and fish_add_path "$HOME/Library/Application Support/JetBrains/Toolbox/scripts"
-fish_add_path $HOME/.npm-global/bin
-fish_add_path $HOME/.nimble/bin
-fish_add_path $HOME/.dotnet/bin
-fish_add_path $CARGO_HOME/bin
-fish_add_path $(go env GOPATH)/bin
-fish_add_path $HOME/.local/bin
-fish_add_path /Applications/Ghostty.app/Contents/MacOS
+# OS-specific environment
+if test $machine_name = Darwin
+    set -gx TERM xterm-256color
+end
 
-# Fish functions
+# Path additions that depend on environment variables
+fish_add_path -g \
+    $PNPM_HOME \
+    $CARGO_HOME/bin \
+    $BUN_INSTALL/bin
+
+# General Settings
+set -U fish_greeting ""  # Universal variable to persist greeting removal
+
+# Aliases
+alias cl='clear'
+alias ..='cd ..'
+alias ls='lsd'
+alias cat='bat'
+alias cd='z'
+alias vim='nvim'
+alias lg='lazygit'
+
+# Git Aliases
+alias gbra='git branch'
+alias gdif='git diff --no-prefix'
+alias glog='git log --color --oneline'
+alias gst='git status'
+alias gwl='git worktree list'
+alias gwa='git worktree add'
+alias gwr='git worktree remove'
+alias gwp='git worktree prune'
+
+# Initialization
+zoxide init fish | source
+COMPLETE=fish jj | source
 fish_ssh_agent
 
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
-# hydro
-set -g hydro_color_pwd "ebbcba"
-set -g hydro_color_git "c4a7e7"
-set -g hydro_color_error "eb6f92"
-set -g hydro_color_prompt "9ccfd8"
-set -g hydro_color_duration "c4a7e7"
+# Hydro Prompt Configuration
+set -g hydro_color_pwd ebbcba
+set -g hydro_color_jj c4a7e7
+set -g hydro_color_error eb6f92
+set -g hydro_color_prompt 9ccfd8
+set -g hydro_color_duration c4a7e7
 set -g hydro_multiline true
 set -g hydro_symbol_prompt "➜"
-
-# jj
-jj util completion fish | source
-
-# pnpm
-set -gx PNPM_HOME "/Users/trmcnvn/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
+set -g hydro_symbol_jj_conflict "💥"
+set -g hydro_symbol_jj_divergent "🚧"
+set -g hydro_symbol_jj_hidden "👻"
+set -g hydro_symbol_jj_immutable "🔒"
