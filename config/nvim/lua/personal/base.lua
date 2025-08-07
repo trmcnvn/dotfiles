752f3401f32a517daa908123f88d6d95cf52c50f
trmcnvn <hello@trmcnvn.dev>
2025-08-07 16:58:52 +1000

    Changes

--- config/nvim/lua/personal/base.lua
+++ config/nvim/lua/personal/base.lua
@@ -48,9 +48,9 @@ vim.opt.fillchars = { eob = " " }
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.smarttab = true
 vim.opt.breakindent = true
 vim.opt.linebreak = true
@@ -150,9 +150,9 @@ vim.api.nvim_create_autocmd("BufRead", {
 	callback = function()
 		vim.schedule(function()
 			vim.opt_local.expandtab = true
-			vim.opt_local.tabstop = 4
-			vim.opt_local.shiftwidth = 4
-			vim.opt_local.softtabstop = 4
+			vim.opt_local.tabstop = 2
+			vim.opt_local.shiftwidth = 2
+			vim.opt_local.softtabstop = 2
 		end)
 	end,
 })
